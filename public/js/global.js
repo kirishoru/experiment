5a1225d5de2aba3e853b8b48dbfba1334e47835d
kirishoru <kirishoru@gmail.com>
2015-03-09 15:21:32 -0700

    merged Experiment with kirishoru.io
    Still need to fix places.html, frame in map, adjust styles, place map
    marker popover, etc.
    Still need to rename to kirishoru.io and host on heroku.

--- public/js/global.js
+++ public/js/global.js
@@ -21,14 +21,14 @@ $(document).ready(function () {
 var map = new L.Map('map').addLayer(new L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png"))
         .setView(new L.LatLng(45.51, -122.67), 9);
 
-//var mapIcon = L.icon({
-//        iconUrl: '../img/finger.png',
-//        iconSize: [40, 25], // size of the icon
-//        shadowSize: [0, 0], // size of the shadow
-//        iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
-//        shadowAnchor: [0, 0], // the same for the shadow
-//        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-//    });
+var mapIcon = L.icon({
+        iconUrl: '../img/finger.png',
+        iconSize: [40, 25], // size of the icon
+        shadowSize: [0, 0], // size of the shadow
+        iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
+        shadowAnchor: [0, 0], // the same for the shadow
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
 
 // Functions =============================================================
 
@@ -37,8 +37,8 @@ function mapInit() {
     $.getJSON('/placelist', function (data1) {
         var placeData = data1;
         for (var i = 0; i < placeData.length; i++) {
-//            L.marker([placeData[i].lat, placeData[i].lng], {icon: mapIcon}).addTo(map);
-			   L.marker([placeData[i].lat, placeData[i].lng]).addTo(map);
+            L.marker([placeData[i].lat, placeData[i].lng], {icon: mapIcon}).addTo(map);
+//			   L.marker([placeData[i].lat, placeData[i].lng]).addTo(map);
         };
 		 
         map.setView(new L.LatLng(placeData[0].lat, placeData[0].lng), 9);
---
+++ public/js/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+	$('#jp').change(function () {
+		if ($(this).val() === "2") {
+			$('.jp7').css('display', 'inline-block');
+		} else {
+			$('.jp7').css('display', 'none');
+		}
+	});
+
+	$('button').click(function () {
+		clearResults();
+		var $measurements = 0;
+		var $bodyFat1 = 0;
+
+		var $age = +$('#age').val();
+		var $weight = +$('#weight').val();
+		var $chest = +$('#chest').val();
+		var $abdominal = +$('#abdominal').val();
+		var $thigh = +$('#thigh').val();
+		var $subscap = +$('#subscap').val();
+		var $tricep = +$('#tricep').val();
+		var $suprail = +$('#suprail').val();
+		var $midaxil = +$('#midaxil').val();
+
+		function barChrt() {
+			var barHeight = 50;
+			var zults = [{
+					"FM": Math.round(($weight * $bodyFat1), 0) / 100,
+					"LM": Math.round(($weight - ($weight * $bodyFat1) / 100) * 100) / 100
+				}]
+			var x = d3.scale.linear().domain([0, (zults[0].LM + zults[0].FM)]).range([0, $('.results').width()]);
+			var chart = d3.select(".chartBar").attr("width", $('.results').width()).attr("height", barHeight+20);
+			var groups = chart.selectAll("g").data(zults).enter().append("g");
+			var Frects = groups.append("rect");
+			var Lrects = groups.append("rect");
+			var Ftext = groups.append("text");
+			var Ltext = groups.append("text");
+
+			Frects.attr("y", 0).attr("x", 0).attr("width", 0).attr("height", barHeight).attr("fill", "orangered")
+				.transition().attr("width", function () {return x(zults[0].FM);}).duration(750).delay(100);
+
+			Lrects.attr("y", 0).attr("x", 0).attr("width", 0).attr("height", barHeight).attr("fill", "#2F66FC")
+				.transition().attr("width", function () {return x(zults[0].LM);}).attr("x", function () {return x(zults[0].FM) + 1;}).duration(750).delay(100);
+
+			Ftext.attr("x", 5).attr("y", (barHeight / 2) + 35).text("FM: " + zults[0].FM + "lbs").attr("dy", ".35em");
+			
+			Ltext.attr("x", $('.results').width() - 90).attr("y", (barHeight / 2) + 35).text("LM: " + zults[0].LM + "lbs").attr("dy", ".35em").attr("id", "lm");
+//			Ltext.attr("x", function () {return x(zults[0].FM) + 6;}).attr("y", (barHeight / 2) + 35).text("LM: " + zults[0].LM + "lbs").attr("dy", ".35em").attr("id", "lm");
+
+		};
+
+
+		if ($('#mf').val() === "1") { //MALE
+			if ($('#jp').val() === "2") { //JP7
+				//using measurements 12,18,9,7,20,6,15, with age 38 and weight 176; results bf%:13.76 fm:24.30lbs
+				$measurements = $chest + $abdominal + $thigh + $subscap + $tricep + $suprail + $midaxil;
+				$bodyFat1 = Math.round((4.95 / (1.112 - (0.00043499 * $measurements) + (0.00000055 * (Math.pow($measurements, 2)) - (0.00028826 * $age))) - 4.5) * 10000) / 100;
+				$('.results').append("<div class='bf'><p>Body Fat: " + $bodyFat1 + "%</p></div>");
+				barChrt();
+			} else { //JP3
+				//using measurements 12,18,9, with age 38 and weight 176; results bf%:12.71 fm:22.44lbs
+				$measurements = $chest + $abdominal + $thigh;
+				$bodyFat1 = Math.round((4.95 / (1.10938 - (0.0008267 * $measurements) + (0.0000016 * (Math.pow($measurements, 2)) - (0.0002574 * $age))) - 4.5) * 10000) / 100;
+				$('.results').append("<div class='bf'><p>Body Fat: " + $bodyFat1 + "%</p></div>");
+				barChrt();
+			}
+		} else { //FEMALE
+			if ($('#jp').val() === "2") { //JP7
+				//using measurements 12,18,9,7,20,6,15, with age 38 and weight 176; results bf%:13.76 fm:24.30lbs
+				$measurements = $chest + $abdominal + $thigh + $subscap + $tricep + $suprail + $midaxil;
+				$bodyFat1 = Math.round((4.95 / (1.097 - (0.00046971 * $measurements) + (0.00000056 * (Math.pow($measurements, 2)) - (0.00012828 * $age))) - 4.5) * 10000) / 100;
+				$('.results').append("<div class='bf'><p>Body Fat: " + $bodyFat1 + "%</p></div>");
+				barChrt();
+			} else { //JP3
+				//using measurements 12,18,9, with age 38 and weight 176; results bf%:12.71 fm:22.44lbs
+				$measurements = $chest + $abdominal + $thigh;
+				$bodyFat1 = Math.round((4.95 / (1.0994921 - (0.0009929 * $measurements) + (0.0000023 * (Math.pow($measurements, 2)) - (0.0001392 * $age))) - 4.5) * 10000) / 100;
+				$('.results').append("<div class='bf'><p>Body Fat: " + $bodyFat1 + "%</p></div>");
+				barChrt();
+			}
+		}
+
+	});
+
+	$('#btn2').click(clearResults);
+
+});
+
+function clearResults() {
+	$('.bf').remove();
+	$('svg g').remove();
+	$('svg svg').remove();
+};
