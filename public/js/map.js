95f97bde4238553f7ed525f296f590d0a9235230
kirishoru <kirishoru@gmail.com>
2015-03-10 11:31:50 -0700

    map functional with new buttons

--- public/js/map.js
+++ public/js/map.js
@@ -7,16 +7,20 @@ var placeData = [];
 // DOM Ready =============================================================
 $(document).ready(function () {
 
-    mapInit();
-    // Add Place button click
-    $('#btnAddPlace').on('click', addPlace);
+	mapInit();
+	// Add Place button click
+	$('#btnAddPlace').on('click', addPlace);
+	$('.ratebtn').on('click', function(){
+		$('.ratebtn').removeClass('active');
+		$(this).addClass('active');
+	});
 
 });
 
 // Map Settings ========================================================
 
 var map = new L.Map('map').addLayer(new L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png"))
-        .setView(new L.LatLng(45.51, -122.67), 9);
+	.setView(new L.LatLng(45.51, -122.67), 9);
 
 //var mapIcon = L.icon({
 //        iconUrl: '../img/finger.png',
@@ -31,71 +35,67 @@ var map = new L.Map('map').addLayer(new L.tileLayer("http://{s}.tile.osm.org/{z}
 
 //Populate Map
 function mapInit() {
-    $.getJSON('/placelist', function (data1) {
-        var placeData = data1;
-        for (var i = 0; i < placeData.length; i++) {
-//            L.marker([placeData[i].lat, placeData[i].lng], {icon: mapIcon}).addTo(map);
-			   L.marker([placeData[i].lat, placeData[i].lng]).addTo(map);
-        };
-		 
-        map.setView(new L.LatLng(placeData[0].lat, placeData[0].lng), 9);
-    });
+	$.getJSON('/placelist', function (data1) {
+		var placeData = data1;
+		for (var i = 0; i < placeData.length; i++) {
+			//            L.marker([placeData[i].lat, placeData[i].lng], {icon: mapIcon}).addTo(map);
+			L.marker([placeData[i].lat, placeData[i].lng]).addTo(map);
+		};
+
+		map.setView(new L.LatLng(placeData[0].lat, placeData[0].lng), 9);
+	});
 };
 
 // Add Place
 function addPlace(event) {
-    event.preventDefault();
-    var rateVal = 0;
-    rateVal = $('button[name=rate]:active').val();
+	event.preventDefault();
+	var rateVal = 0;
+	rateVal = $('.active').val();
+	// Check and make sure a location is entered and a rate is selected
+	if (rateVal > 0 && $('#inputLocation').val() !== '') {
 
-    // Check and make sure a location is entered and a rate is selected
-    if (rateVal > 0 & $('inputLocation').val() !== '') {
+		// insert the geocoder here
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({
+			'address': $('#inputLocation').val()
+		}, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				var userLat = (results[0].geometry.location.k);
+				var userLng = (results[0].geometry.location.D);
+			}
 
-        // insert the geocoder here
-        var geocoder = new google.maps.Geocoder();
-        geocoder.geocode({
-            'address': $('#inputLocation').val()
-        }, function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                var userLat = (results[0].geometry.location.k);
-                var userLng = (results[0].geometry.location.D);
-            }
-			  
-            // compile Place info into one object
-            var newPlace = {
-                'location': $('#inputLocation').val(),
-                'lat': userLat,
-                'lng': userLng,
-                'timestamp': Date.now(),
-                'rate': $('button[name=rate]:active').val(),
-                'comment': $('#comment').val()
-                
-            }
-//				console.log(newPlace);
-            // Use AJAX to post the object to the add service
-            $.ajax({
-                type: 'POST',
-                data: JSON.stringify(newPlace),
-                url: 'https://api.mongolab.com/api/1/databases/effthisplace/collections/placelist?apiKey=kWDQt_LhOcoqnD3vdPCx_7OthM_5TOEJ',
-                contentType: 'application/json'
-            }).done(function (response) {
+			// compile Place info into one object
+			var newPlace = {
+					'location': $('#inputLocation').val(),
+					'lat': userLat,
+					'lng': userLng,
+					'timestamp': Date.now(),
+					'rate': $('.active').val(),
+					'comment': $('#comment').val()
 
-                    // Clear the form inputs
-                    $('.form-control').val('');
-                    $('button').prop('active', function () {
-                        return this.getAttribute('active') == 'active';
-                    });
+				}
+				// Use AJAX to post the object to the add service
+			$.ajax({
+				type: 'POST',
+				data: JSON.stringify(newPlace),
+				url: 'https://api.mongolab.com/api/1/databases/effthisplace/collections/placelist?apiKey=kWDQt_LhOcoqnD3vdPCx_7OthM_5TOEJ',
+				contentType: 'application/json'
+			}).done(function (response) {
 
-                    // Update the table
-                    mapInit();
+				// Clear the form inputs
+				$('.form-control').val('');
+				$('.active').removeClass('active');
 
-            });
+				// Update the table
+				mapInit();
 
-        });
+			});
 
-    } else {
-        // error out
-        alert('Please add a location and a rating');
-        return false;
-    }
+		});
+
+	} else {
+		// error out
+		alert('Please add a location and a rating');
+		return false;
+	}
 };
