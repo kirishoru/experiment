e8cdffede477253b983f7b67d86a706c34c668f2
Caleb Barger <caleb.barger@hdrinc.com>
2015-03-13 16:56:55 -0700

    .

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-	$('#jp').change(function () {
+	
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-21878389-2', 'auto');
+  ga('send', 'pageview');
+
+    
+    $('#jp').change(function () {
 		if ($(this).val() === "2") {
 			$('.jp7').css('display', 'inline-block');
 		} else {
