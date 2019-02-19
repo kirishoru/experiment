33166f2c336f158f97d73094d891571f99e85a04
kirishoru <kirishoru@gmail.com>
2019-02-19 15:25:42 -0800

    adding Relic support

--- public/js/resume.js
+++ public/js/resume.js
@@ -1,7 +1,8 @@
 var header = document.querySelector('header');
 var section = document.querySelector('section');
 var scrollList = document.getElementById('scroll-list');
-var requestURL = '../../public/js/resume2.json';
+var requestURL = '../../public/js/relics.json';
+// var requestURL = '../../public/js/resume2.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'text';
